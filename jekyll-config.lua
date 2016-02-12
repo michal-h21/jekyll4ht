9c4b5b00802e3981be98a19b471cbec9dba55a3e
Michal Hoftich <michal.h21@gmail.com>
2016-02-12 12:20:25 +0100

    Search the config file also in HOME or XDG_CONFIG_HOME

--- jekyll-config.lua
+++ jekyll-config.lua
@@ -6,14 +6,32 @@ function file_exists(name)
 	if f~=nil then io.close(f) return true else return false end
 end
 
-local find_config = function()
+
+local xdg_config  = function(filename)
+  local xdg = os.getenv("XDG_CONFIG_DIR")
+  local home = os.getenv("HOME")
+  if xdg then
+    local fn = xdg .. "/jekyll4ht/".. filename
+    if file_exists(fn) then
+      return fn
+    end
+  end
+  if home then
+    local fn = home .."/"..filename
+    if file_exists(fn) then
+      return fn
+    end
+  end
+end
+
+local find_config = function(filename)
 	local current =  lfs.currentdir()
 	local path = {}
 	current:gsub("([^/]+)", function(s) table.insert(path,s) end)
 	local begin = os.type == "windows" and "" or "/"
 	for i = #path, 1, -1 do
-		local fn =begin .. table.concat(path,"/") .. "/jekyll4ht.conf"
-		print("testing",fn)
+		local fn =begin .. table.concat(path,"/") .. "/".. filename
+		-- print("testing",fn)
 		if file_exists(fn) then return fn end
 		table.remove(path)
 	end
@@ -36,6 +54,7 @@ local function load_config(filename, default)
 end
 --]]
 
-m.find_config = find_config
-m.load_config = lip.load
+m.find_config     = find_config
+m.find_xdg_config = xdg_config
+m.load_config     = lip.load
 return m
