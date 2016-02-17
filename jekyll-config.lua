1824bdd29b0dde9c7ee92fe7a8d329d1267e8f80
Michal Hoftich <michal.h21@gmail.com>
2016-02-17 14:50:43 +0100

    Remove unused lip module

--- jekyll-config.lua
+++ jekyll-config.lua
@@ -1,6 +1,5 @@
 local m = {}
 local mkutils = require "mkutils"
-local lip = require "lip"
 function file_exists(name)
 	local f=io.open(name,"r")
 	if f~=nil then io.close(f) return true else return false end
